3fddc20be0e3ce46d79f6ec219c3e909b2d6a72e
Vendaarlia <94086273+Vendaarlia@users.noreply.github.com>
2024-02-27 14:12:12 +0700

    portal build

---
+++ src/pages/Portal/index.tsx
@@ -0,0 +1,24 @@
+
+
+export default function Portal() {
+    return (
+        <>
+        <section className="PRTL">
+            <div className="ul-PRTL">
+                <div className="crd-PRTL">
+                    <div className="li-PRTL">
+                        <div className="li-PRTL OV"></div>
+                        <div className="li-PRTL LF"></div>
+                        <div className="li-PRTL RG"></div>
+                        <div className="li-PRTL BG-PRTL">
+                            <a href="http://">TELEGRAM</a> <br />
+                            <a href="http://">LinkedIn</a> <br />
+                            <a href="http://">EMAIL</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        </>
+    )
+}
