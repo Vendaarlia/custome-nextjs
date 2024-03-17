a3b9c7f5cbf2e2c0a28ca8740267a951330d2517
Vendaarlia <94086273+Vendaarlia@users.noreply.github.com>
2024-03-17 23:06:56 +0700

    inject @keyframe anim

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,10 +2,14 @@
 
 const NotFoundPage = () => {
   return (
-    <div className="notfoundpage">
+    <div className="wrapper-notfound">
+      <div className="overly">
+      <div className="notfoundpage">
       <h1>UNDER CONQUERING</h1>
-        <p>We are currently breaking this crypt to make it safe to explore, so be patient, traveler 🙏🙏🙏</p>
+        <p>We are currently breaking this crypt to make it safe to explore, so be patient, Traveler<br />🙏🙏🙏</p>
         <h4>404 - Page Not Found</h4>
+    </div>
+      </div>
     </div>
   );
 };
